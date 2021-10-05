00f3e670e9753731553e6ed3b43756f427dfb700
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-10-05 16:30:04 +0530

    Added: Connection to the dataBase

--- src/main/java/com/bridgelabz/EmpPayRollDBService.java
+++ src/main/java/com/bridgelabz/EmpPayRollDBService.java
@@ -1,7 +1,39 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 public class EmpPayRollDBService {
+    /**
+     * Declaring Main Method Here
+     * For Getting The Connection Of DataBase
+     */
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Payroll DataBase JDBC");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?userSSL=false";
+        String userName = "root";
+        String password = "1234";
+        Connection connection;
+        try {
+            System.out.println("connecting to database : " + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection to the Database Successfully! :" + connection);
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Loaded");
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        listDrivers();
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            System.out.println(driverList.nextElement());
+        }
     }
 }
+
